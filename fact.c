b0156b5f8c14675714c70d6e518b621b82a70c8a
pallavikp839 <pallavikp839@gmail.com>
2026-04-06 11:30:29 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-void factorial(){
-	int n, i;
+int factorial(int n){
+	int i;
 unsigned long long fact = 1;
-printf("enter an interger: ");
-scanf("%d", &n);
-
     if(n<0) 
-	    printf("enter factorail of a negetive number doesnt exist,");
-    else{
-	    for (i=1; i<=n; ++i){
-		    fact *= i;
+	    printf("factorail not defined for negetive numbers\n");
+		return 0;
+}
+	    for (i=1; i<=n; i++){
+		    fact = fact * i;
 	    }
 	    printf("factorial of %d = %llu", n, fact);
-    }
-   // return 0;
+  return 0;
 }
