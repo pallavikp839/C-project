e552df1457923e0d52e984dd0b227e855e8d5cda
pallavikp839 <pallavikp839@gmail.com>
2026-04-06 11:37:23 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
-main() {
-	biggest3();
-	factorial();
+#include <stdio.h>
+int biggest3(int a , int b, int c);
+int factorial(int n);
+int main() {
+	biggest3(10, 20, 30);
+	factorial(5);
+	return 0;
 }
