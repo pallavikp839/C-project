70fadca7bd39abd283127923e7af50de6867100c
pallavikp839 <pallavikp839@gmail.com>
2026-04-06 11:47:06 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 int biggest3(int a, int b, int c) {
 
     // Finding max using compound expressions
     if (a >= b && a >= c)
-        printf("%d is the largest number.", a);
+        printf("%d is the largest number", a);
     else if (b >= a && b >= c)
-        printf("%d is the largest number.", b);
+        printf("%d is the largest number", b);
     else
-        printf("%d is the largest number.", c);
+        printf("%d is the largest number", c);
 
     return 0;
 }
